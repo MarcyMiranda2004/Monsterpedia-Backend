87b41f78fc87eb3ae998fc4895b3d94344950f74
Marcello Miranda <marcellomiranda2004@gmail.com>
2025-08-12 12:15:18 +0200

    Initial commit

---
+++ src/main/java/Monsterpedia/it/Monsterpedia/MonsterpediaApplication.java
@@ -0,0 +1,13 @@
+package Monsterpedia.it.Monsterpedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterpediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterpediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Monsterpedia/it/Monsterpedia/MonsterpediaApplicationTests.java
@@ -0,0 +1,13 @@
+package Monsterpedia.it.Monsterpedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterpediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
